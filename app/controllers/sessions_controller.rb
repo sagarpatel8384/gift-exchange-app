3e5dec1e0aed16fd89179b72161a8a472f295221
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-03-16 13:09:38 -0400

    Update Homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:login_success] = "Logged in."
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   skip_before_action :authorized?
   def index
+    if logged_in?
+      render plain: 'Logged In'
+    else
+      render :index
+    end
   end
 end
