0eeee06240bf366b25816493a6d7448b5df8c5a7
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-03-17 19:13:44 -0400

    Add Profile Picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    url = "http://gravatar.com/avatar/#{gravatar_id}.jpg?s=#{size}"
+  end
 end
