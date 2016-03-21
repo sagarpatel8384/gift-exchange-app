3c50748a5fd9655c0173272984523d993388f2b2
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-03-21 13:03:17 -0400

    Update Action Mailer URL

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,20 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url = 'http://gif-club.tumblr.com/'
+    @url = 'https://lit-atoll-85487.herokuapp.com/'
     mail(to: @user.email, subject: "Welcome to GIFTdotCOM.com")
   end
 
   def new_group_email(user, group)
     @user = user
     @group = group
-    @url = 'http://gif-club.tumblr.com/'
+    @url = 'https://lit-atoll-85487.herokuapp.com/'
     mail(to: @user.email, subject: "You created a new group on GIFTdotCOM.com")
   end
 
   def invitation_email(email, group)
     @group = group
-    @url = 'http://gif-club.tumblr.com/'
+    @url = 'https://lit-atoll-85487.herokuapp.com/'
     mail(to: email, subject: "#{group.admin.first_name} wants you to join a GIFTdotCOM.com")
   end
 
@@ -22,7 +22,7 @@ class UserMailer < ApplicationMailer
     @group = group
     @giver = giver
     @receiver = receiver
-    @url = 'http://gif-club.tumblr.com/'
+    @url = 'https://lit-atoll-85487.herokuapp.com/'
     mail(to: giver.email, subject: "You're giving a gift to #{receiver.full_name} in #{group.name}")
   end
 end
