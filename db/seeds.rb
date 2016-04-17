ddb2d0ecfa6f04ec5bca80d8914c66cab0e915cd
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-04-17 18:24:17 -0400

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-require 'database_cleaner'
-DatabaseCleaner.strategy = :truncation
-DatabaseCleaner.clean
-
 # STATES & COUNTRIES
 
 states_db = %w(AK AL AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY)
