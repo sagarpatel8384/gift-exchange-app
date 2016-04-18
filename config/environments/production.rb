6f702d1c9bf7ac5e2f4d32d1ce6a985fc3b6c1d3
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-04-18 18:18:13 -0400

    Precompile Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 Rails.application.configure do
   config.cache_classes = true
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compile = true
   config.assets.digest = true
 
